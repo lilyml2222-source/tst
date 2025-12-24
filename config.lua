3219f62e50da0e28133829e9f62fb7d53064785f
lilyml2222-source <lilyml2222@gmail.com>
2025-12-25 00:50:14 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,28 @@
+-- CONFIG.LUA - Configuration & Variables
+local Config = {}
+
+-- GitHub Settings
+Config.GitHubUser = "crystalknight-svg"
+Config.Branch = "main"
+
+-- Mount List
+Config.MountList = {
+    "Mount Funny", "Mount Yahayuk", "Mount Gemi", "Mount Age",
+    "Mount Runia", "Mount Yubi", "Mount Anjir", "Mount Velora",
+    "Mount Freestyle", "Mount Aethria", "Mount Luna",
+    "Mount Wasabi", "Mount Kita"
+}
+
+-- Internal Variables (akan diakses oleh core.lua)
+Config.TASDataCache = {}
+Config.isCached = false
+Config.isPlaying = false
+Config.isLooping = false
+Config.SavedCP = 0
+Config.SavedFrame = 1
+Config.END_CP = 1000
+Config.CurrentRepoURL = ""
+Config.FlipOffset = 0
+Config.SpeedMultiplier = 1
+
+return Config
